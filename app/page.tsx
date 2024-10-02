11f757b50bea6363be32b8c74970686a2e6c52d0
Santiago Pintos <spintos@outlook.es>
2024-10-02 13:07:34 -0300

    feat: Add interface to upload a picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
-    <></>
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type="file"/>
+        <Button type="submit">Cargar</Button>
+      </div>
+    </div>
   );
 }
