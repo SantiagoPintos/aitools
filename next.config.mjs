76e7a62ccb6167b83fd5db479bb8e44638d104ad
Santiago Pintos <spintos@outlook.es>
2024-10-02 16:53:45 -0300

    First prototype

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+                port: '',
+                pathname: '/photos/5965592/pexels-photo-5965592.jpeg',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
