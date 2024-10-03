05cc6ffa33b92e6f833ab4a6c1819f59b941b3ef
Santiago Pintos <spintos@outlook.es>
2024-10-03 20:15:25 -0300

    feat: add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react';
 import dynamic from 'next/dynamic';
+import Navbar from '../components/Navbar';
 
 const DynamicImageProcessor = dynamic(
   () => import('../components/ImageProcessor').then(module => module.default), 
@@ -10,7 +11,7 @@ const DynamicImageProcessor = dynamic(
 const Home: React.FC = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center my-8 ">Image Background Remover</h1>
+      <Navbar />
       <DynamicImageProcessor />
     </div>
   );
