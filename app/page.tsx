baa5eeccec80194e564d51900138ac1389bb8763
Santiago Pintos <spintos@outlook.es>
2024-10-04 15:24:04 -0300

    refactor: clear session storage on component mount

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import Navbar from '../components/Navbar';
 import Landing from '../components/Landing';
@@ -10,6 +10,9 @@ const DynamicImageProcessor = dynamic(
 );
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    sessionStorage.clear();
+  }, []);
   return (
     <div>
       <Navbar />
