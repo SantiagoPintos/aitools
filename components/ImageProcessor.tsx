ba1b11e8cf8b58a5a5d8c7ca8db2ce568acddd1c
Santiago Pintos <spintos@outlook.es>
2024-10-04 16:09:26 -0300

    refactor: improve image processing logic and error handling

--- components/ImageProcessor.tsx
+++ components/ImageProcessor.tsx
@@ -55,7 +55,6 @@ const ImageProcessor: React.FC = () => {
     const storedImage = sessionStorage.getItem('image');
     if (storedImage) {
       setOriginalImage(storedImage);
-      await removeBackground();
     }
   }
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -75,35 +74,37 @@ const ImageProcessor: React.FC = () => {
   }, [originalImage]);
 
   const removeBackground = async () => {
-    setIsProcessing(true);
+    try {
+      setIsProcessing(true);
 
-    if (!originalImage) {
-      console.error('No image to process');
-      setIsProcessing(false);
-      return;
-    }
-    const image = await RawImage.fromURL(originalImage);
-
-    const { pixel_values } = await processorRef.current(image);
-    const { output } = await modelRef.current({ input: pixel_values });
-    const mask = await RawImage.fromTensor(output[0].mul(255).to('uint8')).resize(image.width, image.height);
-
-    const canvas = document.createElement('canvas');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    const ctx = canvas.getContext('2d');
-
-    if (ctx) {
-      ctx.drawImage(image.toCanvas(), 0, 0);
-      const pixelData = ctx.getImageData(0, 0, image.width, image.height);
-      for (let i = 0; i < mask.data.length; ++i) {
-        pixelData.data[4 * i + 3] = mask.data[i];
+      if (!originalImage) {
+        throw new Error('No image to process');
       }
-      ctx.putImageData(pixelData, 0, 0);
-    }
 
-    setProcessedImage(canvas.toDataURL());
-    setIsProcessing(false)
+      const image = await RawImage.fromURL(originalImage);
+      const { pixel_values } = await processorRef.current(image);
+      const { output } = await modelRef.current({ input: pixel_values });
+      const mask = await RawImage.fromTensor(output[0].mul(255).to('uint8')).resize(image.width, image.height);
+      const canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const ctx = canvas.getContext('2d');
+
+      if (ctx) {
+        ctx.drawImage(image.toCanvas(), 0, 0);
+        const pixelData = ctx.getImageData(0, 0, image.width, image.height);
+        for (let i = 0; i < mask.data.length; ++i) {
+          pixelData.data[4 * i + 3] = mask.data[i];
+        }
+        ctx.putImageData(pixelData, 0, 0);
+      }
+
+      setProcessedImage(canvas.toDataURL());
+    } catch (error) {
+      console.error('Error processing image:', error);
+    } finally {
+      setIsProcessing(false)
+    }
   };
 
   const resetImages = () => {
