bbde538468139b54b7e386a4b4cf04cba932e19d
Santiago Pintos <spintos@outlook.es>
2024-10-04 15:23:26 -0300

    refactor: add new page component for removing images

---
+++ app/remove/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import ImageProcessor from "@/components/ImageProcessor";
+import Navbar from "@/components/Navbar";
+
+export default function Page() {
+    return (
+        <>
+            <Navbar />
+            <ImageProcessor/>
+        </>
+    );
+}
