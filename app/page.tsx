696306c9e484bc56910b84b32c86c694f6fff6ff
Santiago Pintos <spintos@outlook.es>
2024-10-04 18:47:57 -0300

    Refactor: Remove dynamic import of ImageProcessor component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 import React, { useEffect } from 'react';
-import dynamic from 'next/dynamic';
 import Navbar from '../components/Navbar';
 import Landing from '../components/Landing';
 
-const DynamicImageProcessor = dynamic(
-  () => import('../components/ImageProcessor').then(module => module.default), 
-  { ssr: false }
-);
 
 const Home: React.FC = () => {
   useEffect(() => {
