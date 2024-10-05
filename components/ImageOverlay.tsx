487bc2da5352668370b5aa0a7b1881bdd923fafa
Santiago Pintos <spintos@outlook.es>
2024-10-05 13:57:50 -0300

    Refactor: Add ImageOverlay component

---
+++ components/ImageOverlay.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+
+interface ImageOverlayProps {
+  originalSrc: string;
+  processedSrc: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+
+export default function ImageOverlay({ 
+    originalSrc, 
+    processedSrc, 
+    alt,
+    width = 300, 
+    height = 300, 
+    }: ImageOverlayProps) {
+    return (
+        <div 
+            className={`relative mx-auto`} 
+            style={{ width: `${width}px`, height: `${height}px` }}
+        >
+        <div className="absolute inset-0 overflow-hidden">
+          <Image
+            src={originalSrc}
+            alt={`Original ${alt}`}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div 
+          className="absolute inset-0 overflow-hidden" 
+          style={{ clipPath: 'inset(0 50% 0 0)' }}
+        >
+          <Image
+            src={processedSrc}
+            alt={`Processed ${alt}`}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div className="absolute inset-y-0 left-1/2 w-0.5 bg-white"></div>
+      </div>
+    )
+}
