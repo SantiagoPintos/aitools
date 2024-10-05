cd9a22eeff93f3d065d7cb3c91413bdde6511c58
Santiago Pintos <spintos@outlook.es>
2024-10-05 20:30:53 -0300

    Refactor: Replace img tags with Next.js Image component in ImageOverlay

--- components/ImageOverlay.tsx
+++ components/ImageOverlay.tsx
@@ -21,22 +21,26 @@ export default function ImageOverlay({
             style={{ width: `${width}px`, height: `${height}px` }}
         >
         <div className="absolute inset-0 overflow-hidden">
-          <img
+          <Image
             src={originalSrc}
             alt={`Original ${alt}`}
             height={height}
             width={width}
+            priority={true}
+            unoptimized={true}
           />
         </div>
         <div 
           className="absolute inset-0 overflow-hidden" 
           style={{ clipPath: 'inset(0 50% 0 0)' }}
         >
-          <img
+          <Image
             src={processedSrc}
             alt={`Processed ${alt}`}
             height={height}
             width={width}
+            priority={true}
+            unoptimized={true}
           />
         </div>
         <div className="absolute inset-y-0 left-1/2 w-0.5 bg-white"></div>
