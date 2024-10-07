5ab66f2a0b2df5ab25726a94d2e5ad0448194097
Santiago Pintos <spintos@outlook.es>
2024-10-07 20:59:01 -0300

    Refactor: Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,14 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: isProd ? '/image' : '',  // Sustituye 'nombre-del-repositorio' por el nombre de tu repositorio
+    basePath: isProd ? '/image' : '', 
     assetPrefix: isProd ? '/image/' : '',
     webpack: (config, { isServer }) => {
-        // Configuración para WebAssembly
         config.experiments = {
           ...config.experiments,
           asyncWebAssembly: true,
         };
     
-        // Evitar que Next.js intente procesar módulos específicos en el servidor
         if (isServer) {
           config.externals.push({
             '@xenova/transformers': 'commonjs @xenova/transformers',
