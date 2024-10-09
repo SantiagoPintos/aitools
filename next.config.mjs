07ee8e4a8a7dcdd0d4dd987c6a948cf70348a110
Santiago Pintos <spintos@outlook.es>
2024-10-09 12:57:21 -0300

    Refactor: remove unused prop in next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: isProd ? '/' : '', 
-    assetPrefix: isProd ? '/' : '',
     webpack: (config, { isServer }) => {
         config.experiments = {
           ...config.experiments,
