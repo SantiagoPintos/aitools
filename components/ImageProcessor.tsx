ebd09e33f1d94a9007a9ef6ea8d6e21ee3ed4cca
Santiago Pintos <spintos@outlook.es>
2024-10-09 20:06:17 -0300

    Refactor: Add error handling for model and processor loading

--- components/ImageProcessor.tsx
+++ components/ImageProcessor.tsx
@@ -4,7 +4,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import { AutoModel, AutoProcessor, env, RawImage } from '@xenova/transformers';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
-import { Download, ImageIcon, Loader2, RefreshCw, UploadCloud } from 'lucide-react';
+import { Download, ImageIcon, Loader2, RefreshCw, UploadCloud, AlertCircle } from 'lucide-react';
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import ImageUploader from './ImageUploader';
 import { useAppContext } from '@/context/ImageContext';
 
@@ -12,6 +13,7 @@ import { useAppContext } from '@/context/ImageContext';
 const ImageProcessor: React.FC = () => {
   const { originalImage, processedImage, setProcessedImage, setOriginalImage } = useAppContext();
   const [isProcessing, setIsProcessing] = useState(false)
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
   
   const modelRef = useRef<any>(null);
   const processorRef = useRef<any>(null);
@@ -31,32 +33,37 @@ const ImageProcessor: React.FC = () => {
   }
   
   const loadModelAndProcessor = async () => {
-    // WASM settings
-    env.allowLocalModels = false;
-    // Proxy the WASM backend to prevent the UI from freezing
-    env.backends.onnx.wasm.proxy = true;
-    // Enable WASM simd and multi-threading to speed up the inference
-    env.backends.onnx.wasm.simd = true;
-    env.backends.onnx.wasm.numThreads = 4;
-
-    modelRef.current = await AutoModel.from_pretrained('briaai/RMBG-1.4', {
-      config: { model_type: 'custom' },
-    });
-
-    processorRef.current = await AutoProcessor.from_pretrained('briaai/RMBG-1.4', {
-      config: {
-        do_normalize: true,
-        do_pad: false,
-        do_rescale: true,
-        do_resize: true,
-        image_mean: [0.5, 0.5, 0.5],
-        feature_extractor_type: "ImageFeatureExtractor",
-        image_std: [1, 1, 1],
-        resample: 2,
-        rescale_factor: 0.00392156862745098,
-        size: { width: 1024, height: 1024 },
-      }
-    });
+    try{
+      // WASM settings
+      env.allowLocalModels = false;
+      // Proxy the WASM backend to prevent the UI from freezing
+      env.backends.onnx.wasm.proxy = true;
+      // Enable WASM simd and multi-threading to speed up the inference
+      env.backends.onnx.wasm.simd = true;
+      env.backends.onnx.wasm.numThreads = 4;
+  
+      modelRef.current = await AutoModel.from_pretrained('briaai/RMBG-1.4', {
+        config: { model_type: 'custom' },
+      });
+  
+      processorRef.current = await AutoProcessor.from_pretrained('briaai/RMBG-1.4', {
+        config: {
+          do_normalize: true,
+          do_pad: false,
+          do_rescale: true,
+          do_resize: true,
+          image_mean: [0.5, 0.5, 0.5],
+          feature_extractor_type: "ImageFeatureExtractor",
+          image_std: [1, 1, 1],
+          resample: 2,
+          rescale_factor: 0.00392156862745098,
+          size: { width: 1024, height: 1024 },
+        }
+      });
+    } catch (error) {
+      setShowErrorAlert(true);
+      console.error('Error loading model and processor:', error);
+    }
   };
   const checkForStoredImage = async () => {
     if (originalImage) {
@@ -131,38 +138,48 @@ const ImageProcessor: React.FC = () => {
   return (
     <div className="container mx-auto p-4 max-w-4xl">
       <div className="flex flex-col md:flex-row gap-8">
-        <Card className="flex-1">
-          <CardContent className="p-6">
-            {originalImage ? (
-              <h2 className="text-xl font-semibold mb-4 text-center">Processed image</h2>
-            ) : (
-                <div className="flex items-center justify-center mb-4">
-                  <ImageIcon className="h-8 w-8 mr-2" />
-                  <span className="text-lg font-semibold">Upload an Image</span>
+          {showErrorAlert ? (
+            <Alert variant="destructive" className="mb-4">
+              <AlertCircle className="mr-2 h-4 w-4" />
+              <AlertTitle>Error</AlertTitle>
+              <AlertDescription>
+                There was an error loading the model and processor. Please try again later.
+              </AlertDescription>
+            </Alert>
+          ) :
+            <Card className="flex-1">
+              <CardContent className="p-6">
+                {originalImage ? (
+                  <h2 className="text-xl font-semibold mb-4 text-center">Processed image</h2>
+                ) : (
+                    <div className="flex items-center justify-center mb-4">
+                      <ImageIcon className="h-8 w-8 mr-2" />
+                      <span className="text-lg font-semibold">Upload an Image</span>
+                    </div>
+                    )
+                }
+                <div className="aspect-square w-full overflow-hidden rounded-lg">
+                  {processedImage ? (
+                    <img
+                      src={processedImage}
+                      alt="Processed"
+                      className="object-contain w-full h-full"
+                    />
+                  ) : originalImage ? (
+                    <img
+                      src={originalImage}
+                      alt="Original"
+                      className="object-contain w-full h-full"
+                    />
+                  ) : (
+                    <div className="h-full w-full flex items-center justify-center">
+                      <ImageUploader onImageUpload={handleImageUpload} originalImage={originalImage} />
+                    </div>
+                  )}
                 </div>
-                )
-            }
-            <div className="aspect-square w-full overflow-hidden rounded-lg">
-              {processedImage ? (
-                <img
-                  src={processedImage}
-                  alt="Processed"
-                  className="object-contain w-full h-full"
-                />
-              ) : originalImage ? (
-                <img
-                  src={originalImage}
-                  alt="Original"
-                  className="object-contain w-full h-full"
-                />
-              ) : (
-                <div className="h-full w-full flex items-center justify-center">
-                  <ImageUploader onImageUpload={handleImageUpload} originalImage={originalImage} />
-                </div>
-              )}
-            </div>
-          </CardContent>
-        </Card>
+              </CardContent>
+            </Card>
+          }
 
         <div className="flex flex-col gap-4 justify-center md:w-64">
           <Button
