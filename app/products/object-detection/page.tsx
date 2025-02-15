575e81e4516036a569251fc74f2a1504a3338f55
Santiago Pintos <spintos@outlook.es>
2025-02-15 17:39:57 -0300

    feat: add Object Detection page with styles for bounding boxes

---
+++ app/products/object-detection/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ObjectDetection from "@/components/ObjectDetection"
+import './styles.css';
+
+const Home : React.FC = () => {
+    return (
+        <div>
+            <ObjectDetection />
+        </div>
+    );
+}
+
+export default Home;
